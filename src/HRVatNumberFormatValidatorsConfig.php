ec1a7b535b0c1a08ff15466d20fd6a2ebe437dde
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-05 00:35:32 +0400

    [CVFV-45] setup config.

--- src/HRVatNumberFormatValidatorsConfig.php
+++ src/HRVatNumberFormatValidatorsConfig.php
@@ -1,21 +1,22 @@
 <?php
 
-// TODO: namespace
+namespace rocketfellows\HRVatNumberFormatValidatorsConfig;
 
 use arslanimamutdinov\ISOStandard3166\Country;
 use arslanimamutdinov\ISOStandard3166\ISO3166;
 use rocketfellows\CountryVatFormatValidatorInterface\CountryVatFormatValidatorInterface;
+use rocketfellows\HRVatFormatValidator\HRVatFormatValidator;
 use rocketfellows\SpecificCountryVatNumberFormatValidatorsConfig\SpecificCountryVatNumberFormatValidatorsConfig;
 
-class CountryVatNumberFormatValidatorsConfig extends SpecificCountryVatNumberFormatValidatorsConfig
+class HRVatNumberFormatValidatorsConfig extends SpecificCountryVatNumberFormatValidatorsConfig
 {
     public function getCountry(): Country
     {
-        return ISO3166::COUNTRY();
+        return ISO3166::HR();
     }
 
     protected function getDefaultValidator(): CountryVatFormatValidatorInterface
     {
-        return new CountryVatFormatValidator();
+        return new HRVatFormatValidator();
     }
 }
